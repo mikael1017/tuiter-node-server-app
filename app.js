638ef22877f58b6b2b5102d69329180f70534c9f
Jaewoo <jaewoocho1017@gmail.com>
2023-03-26 22:41:19 -0700

    completed backend

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from "express";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from "cors";
 
 const app = express();
 console.log("Starting the server");
+app.use(cors());
 app.use(express.json());
 TuitsController(app);
 HelloController(app);
