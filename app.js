e6de95d8bf2ebfbe40ac25657676daf5eed0fe39
Jaewoo <jaewoocho1017@gmail.com>
2023-03-26 21:34:53 -0700

    completed 5

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+
 const app = express();
 console.log("Starting the server");
 app.use(express.json());
+TuitsController(app);
 HelloController(app);
 UserController(app);
 
