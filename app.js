d054bd6d98bf2037b6780f64bf4acb306697be6b
Jaewoo <jaewoocho1017@gmail.com>
2023-03-26 19:25:48 -0700

    completed until 4.5

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 const app = express();
 console.log("Starting the server");
+app.use(express.json());
 HelloController(app);
 UserController(app);
 
