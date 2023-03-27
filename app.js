68c61b04925505d0195b07a21ce79cf72db124ac
Jaewoo <jaewoocho1017@gmail.com>
2023-03-26 18:59:04 -0700

    completed until 3

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+const app = express();
+console.log("Starting the server");
+HelloController(app);
+UserController(app);
+
+console.log("Started the server");
+
+app.listen(4000);
